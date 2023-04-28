9a9541d04be5b712eecfeeddeecbe5a22959a548
unknown <waqasimalok@gmail.com>
2023-04-29 02:03:38 +0500

    update

---
+++ deom1/js/index.js
@@ -0,0 +1,28 @@
+// NAVIGATION BAR STYLE
+window.onscroll = () => {
+  const nav = document.querySelector("#navigation");
+  if (this.scrollY <= 0) nav.className = "mk-nav";
+  else nav.className = "scroll-navigation";
+};
+
+// COUNTER 
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+   let obj = document.getElementById(id),
+    current = start,
+    range = end - start,
+    increment = end > start ? 1 : -1,
+    step = Math.abs(Math.floor(duration / range)),
+    timer = setInterval(() => {
+     current += increment;
+     obj.textContent = current;
+     if (current == end) {
+      clearInterval(timer);
+     }
+    }, step);
+  }
+  counter("count1", 0, 400, 3000);
+  counter("count2", 100, 50, 2500);
+  counter("count3", 0, 40, 3000);
+ });
+ 
